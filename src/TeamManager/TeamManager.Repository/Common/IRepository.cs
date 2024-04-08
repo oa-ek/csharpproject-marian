c474890364326b3d28d24956a9778f27d2e0588e
Marian Zharchynskyi <marian.zharchynskyi@oa.edu.ua>
2024-04-09 00:20:13 +0300

    Repository with CRUD was added

---
+++ src/TeamManager/TeamManager.Repository/Common/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamManager.Repository.Common
+{
+    public interface IRepository<TEntity, TKey>
+        where TEntity : class, IEntity<TKey>
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> GetAsync(TKey id);
+        Task CreateAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task DeleteAsync(TKey id);
+        Task SaveAsync();
+    }
+}
